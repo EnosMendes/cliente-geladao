2fb4aeaf22374552014612f86f842fccb6d93416
Enos Mendes <enos981077559@gmail.com>
2022-12-11 18:36:18 -0300

    inicia o projeto

---
+++ src/main/java/br/com/geladao/clientegeladao/ClienteGeladaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.geladao.clientegeladao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteGeladaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteGeladaoApplication.class, args);
+	}
+
+}
