b00c5ff2057768478176cd9dd026a61347a82294
Enos Mendes <enos981077559@gmail.com>
2022-12-11 19:46:38 -0300

    configura a parte web

--- src/main/java/br/com/geladao/clientegeladao/ClienteGeladaoApplication.java
+++ src/main/java/br/com/geladao/clientegeladao/ClienteGeladaoApplication.java
@@ -2,9 +2,19 @@ package br.com.geladao.clientegeladao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClienteGeladaoApplication {
+	
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cliente Geladao - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteGeladaoApplication.class, args);
